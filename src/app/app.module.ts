3970066316393aa1919a92946240e7177b1b8b34
“Rene <renesalas821@gmail.com>
2024-01-11 11:02:45 -0600

    Version 1.1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -9,9 +8,10 @@ import { AboutComponent } from './components/about/about.component';
 
 
 
+
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent, 
     NavbarComponent,
     HomeComponent,
     AboutComponent
